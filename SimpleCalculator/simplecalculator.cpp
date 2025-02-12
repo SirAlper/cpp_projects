2a2986ac1db82285ffb105771327d0d82af00515
Alper Han <hanalperhan@gmail.com>
2025-02-12 12:24:44 +0300

    firstcommit

---
+++ SimpleCalculator/simplecalculator.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    float x,y;
+    int operation;
+    float result;
+    cout << "Enter two number: ";
+    cin >> x >> y;
+    cout << "Choose an operator:"<<"\n"
+         << "1: Summation (+)"<<"\n"
+         << "2: Substraction (-)"<<"\n"
+         << "3: Multiplication (x)"<<"\n"
+         << "4: Divide (/)"<<"\n";
+    
+    cin >> operation;
+
+    switch (operation)
+    {
+        case 1: result = x + y;break;
+        case 2: result = x - y;break;
+        case 3: result = x * y;break;
+        case 4: result = x / y;break;
+        default:
+            cout<<"\nPlease enter a valid value"<<"\n"; 
+            cout << "\nEnter a key for exit" <<"\n";
+            cin.get();
+            cin.get(); return 0; 
+    }
+
+    cout << "\nYour result is: "<<result<<"\n";
+    
+    cout << "\nEnter a key for exit" <<"\n";
+    cin.get();
+    cin.get();  
+    return 0;
+}
