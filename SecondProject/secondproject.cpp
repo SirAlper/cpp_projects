50f9f916a40c0744512fae056e7418b5be8b880b
Alper Han <hanalperhan@gmail.com>
2025-02-12 20:29:06 +0300

    commit3

---
+++ SecondProject/secondproject.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    char letter; 
+    cout << "Enter a letter: ";
+    cin >> letter;
+    switch (letter)
+    {
+        case 'a':
+        case 'e':
+        case 'u':
+        case 'o':
+        case 'i': cout << letter << " is a voewl\n";break;
+        default:cout<<letter<<" is a consonant ";
+                cout<<"or not a letter \n";break;
+    }
+    return 0;
+}
